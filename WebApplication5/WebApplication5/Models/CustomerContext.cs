1b7feb5ee034fb320717f687f3bf7853246c4e0d
gregb12 <125223546+gregb12@users.noreply.github.com>
2023-02-14 15:00:38 -0500

    rollback

--- WebApplication5/WebApplication5/Models/CustomerContext.cs
+++ WebApplication5/WebApplication5/Models/CustomerContext.cs
@@ -5,7 +5,7 @@ namespace WebApplication5.Models
     public class AZURE_SQL_CONNECTIONSTRING:DbContext
     {
 
-        public CAZURE_SQL_CONNECTIONSTRING(DbContextOptions<AZURE_SQL_CONNECTIONSTRING> options) : base(options) { }
+        public CustomerContext(DbContextOptions<CustomerContext> options) : base(options) { }
         public DbSet<Customer> Customer { get; set; } = null!;
     }
 }
